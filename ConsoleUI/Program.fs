4fab96f11f36488f1698d8af33a8be0a597c1c81
Vergil Iliev <archturion64@gmail.com>
2020-04-17 23:14:38 +0200

    add run all async functionality

--- ConsoleUI/Program.fs
+++ ConsoleUI/Program.fs
@@ -15,22 +15,32 @@ let mutable CurrentMenu =  ref mainMenu
 
 let ReportResult testName test containerSize =
     printfn "Bench %s complete in: %f ms" testName (test containerSize)
+ 
+let RunAllEntryesAsync (map: Map<int,string * (unit -> unit)>) = 
+    map
+    |> Seq.filter (fun x -> x.Key > 0 && x.Key < Seq.length map - 1) // skip first and last manue entry
+    |> Seq.map (fun (x: KeyValuePair<int,string * (unit -> unit)>) -> async {snd(x.Value)()})
+    |> Async.Parallel
+    |> Async.RunSynchronously |> ignore
+    printfn " ms workflow started"
 
 let hashSetMenu =
     Map.empty.
         Add(0, ("Back", (fun () -> CurrentMenu := mainMenu ) )).
         Add(1, ("Hashset<Tuple>", (fun () -> ReportResult "Hashset<Tuple>" CTBHashSet.DoBenchmarkHashSetTuple containerSize  ) )).
-        Add(2, ("Hashset<ValueTuple>", (fun () -> ReportResult  "Hashset<ValueTuple>" CTBHashSet.DoBenchmarkHashSetValueTuple containerSize )));;
+        Add(2, ("Hashset<ValueTuple>", (fun () -> ReportResult  "Hashset<ValueTuple>" CTBHashSet.DoBenchmarkHashSetValueTuple containerSize ))).
+        Add(3, ("Run all async", (fun () ->  RunAllEntryesAsync  !CurrentMenu)));;
 
 let minMaxMenu =
     Map.empty.
         Add(0, ("Back", (fun () -> CurrentMenu := mainMenu ) )).
         Add(1, ("Array Min Max with LINQ", (fun () -> ReportResult "Array Min Max with LINQ" CTBMinMax.DoBenchmarkMinMaxLinq containerSize ))).
-        Add(2, ("Array Min Max manual", (fun () -> ReportResult "Array Min Max manual" CTBMinMax.DoBenchmarkMinMaxManual containerSize )));;
+        Add(2, ("Array Min Max manual", (fun () -> ReportResult "Array Min Max manual" CTBMinMax.DoBenchmarkMinMaxManual containerSize ))).
+        Add(3, ("Run all async", (fun () ->  RunAllEntryesAsync  !CurrentMenu)));;
 
 let GenerateMenu (map: Map<int,string * _>)=
     printfn "%s Enter a number for your choice of action: %s" Environment.NewLine Environment.NewLine
-    Seq.iteri (fun index (item: KeyValuePair<int, string * _>) -> printfn "  %i: %s" item.Key (fst(item.Value))) map
+    Seq.iter (fun (item: KeyValuePair<int, string * _>) -> printfn "  %i: %s" item.Key (fst(item.Value))) map
     printfn "%s" Environment.NewLine
 
 let TriggerSelection (selection :int) (map : Map<int,string * (unit-> unit)>) = 
